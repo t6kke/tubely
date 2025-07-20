bd4854348f4a26232c0d854b3cd6abf4c817e437
Martin <m.t6kke@gmail.com>
2025-07-20 11:54:27 +0300

    c1 lessons 6 to 9 completed, storing thumbnails into ./assets directory

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"os"
 	"io"
 	"fmt"
+	"mime"
+	"strings"
 	"net/http"
+	//"encoding/base64"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -35,19 +40,21 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	const maxMemory = 10 << 20 // 10 MB using bit shifting
 	r.ParseMultipartForm(maxMemory)
 
-	file, _, err := r.FormFile("thumbnail")
+	file, header, err := r.FormFile("thumbnail")
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
 		return
 	}
 	defer file.Close()
-	media_type := r.Header.Get("Content-Type")
+	//media_type := r.Header.Get("Content-Type")
+	media_type := header.Header.Get("Content-Type")
 
-	byte_data, err := io.ReadAll(file)
+	// part of v1
+	/*byte_data, err := io.ReadAll(file)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Unable to parse the file to byte data", err)
 		return
-	}
+	}*/
 
 	vide_metadata, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -59,14 +66,49 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	tn := thumbnail{
+	// part of v1
+	/*tn := thumbnail{
 		data:      byte_data,
 		mediaType: media_type,
 	}
-	videoThumbnails[videoID] = tn
+	videoThumbnails[videoID] = tn*/
 
-	tn_url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%v", cfg.port, videoID)
+	// v1 storing in memory - old
+	//tn_url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%v", cfg.port, videoID)
+	//vide_metadata.ThumbnailURL = &tn_url
 
+	// v2 storing in db as base64 encoded data - old
+	//base_64_data := base64.StdEncoding.EncodeToString(byte_data)
+	//tn_url := fmt.Sprintf("data:%s;base64,%s", media_type, base_64_data)
+	//vide_metadata.ThumbnailURL = &tn_url
+
+	// v3 storing in ./assets/directory
+	m_type, _, err := mime.ParseMediaType(media_type)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid thumbnain filetype provided", err)
+		return
+	}
+	if m_type != "image/jpeg" && m_type != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Invalid thumbnain filetype provided", err)
+		return
+	}
+	m_type_parts := strings.Split(m_type, "/")
+	file_name := fmt.Sprintf("%s.%s", videoID, m_type_parts[1])
+	file_path := filepath.Join(cfg.assetsRoot, file_name)
+	file_ptr, err := os.Create(file_path)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to store thumbnail", err)
+		return
+	}
+	defer file_ptr.Close()
+	reader := io.Reader(file)
+	writer := io.Writer(file_ptr)
+	_, err = io.Copy(writer, reader)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to store thumbnail", err)
+		return
+	}
+	tn_url := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, file_name)
 	vide_metadata.ThumbnailURL = &tn_url
 
 	err = cfg.db.UpdateVideo(vide_metadata)
